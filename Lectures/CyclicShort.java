e77a407a1259d8d0701c29eed71cd7ef162c4f3c
Abhishek Raj <abhishekraj605912@gmail.com>
2025-01-13 12:11:42 +0530

    Added Cyclic Short

--- Lectures/CyclicShort.java
+++ Lectures/CyclicShort.java
@@ -1,7 +1,28 @@
 package Lectures;
 
+import java.util.Arrays;
+
 public class CyclicShort {
     public static void main(String[] args) {
+        int[] list = {4,3,2,1};
+        sort(list);
+        System.out.println(Arrays.toString(list));
 
+    }
+    static void sort(int[] arr ){
+        int i = 0 ;
+        while ( i < arr.length){
+            int correct = arr[i]-1 ;
+            if ( arr[i] != arr[correct]){
+                swap(arr , i ,correct);
+            }else {
+                i++;
+            }
+        }
+    }
+    static  void swap(int[] arr , int index ,int correct){
+        int temp = arr[correct];
+        arr[correct] = arr[index];
+        arr[index] = temp;
     }
 }
